cd53f3abdc4c1f84ffd511394db2c8c07080049c
azumirojas <azumi.rojas@vallegrande.edu.pe>
2023-11-07 16:47:05 -0500

    crud

---
+++ Crud/app.js
@@ -0,0 +1,36 @@
+var mysql = require('mysql');
+
+var conexion = mysql.createConnection({
+    host: 'localhost',
+    database: 'sakali',
+    user: 'root',
+    password: 'admin'
+});
+conexion.connect(function (error) {
+    if (error) {
+        throw error;
+    } else {
+        console.log('conexion exitosa');
+    }
+});
+//mostrar
+conexion.query('SELECT * from actor', function (error, results, fields){
+    if (error)
+        throw error;
+    results.forEach(result => {
+        console.log(result);
+    });
+})
+//insertar
+conexion.query('INSERT INTO actor (first_name, last_name) VALUES ("Adam", "Smith")', function(error, results){
+    if(error) throw error;
+    console.log('¡Registro Agregado!', results)
+
+});
+//actualizar
+conexion.query('UPDATE actor SET first_name = "Steve", last_name = "Rogers" WHERE id=6 ', function(error, results){
+    if(error)throw error;
+    console.log('¡Registro Actualizado!', results);
+
+});
+conexion.end();
